2d6d26339c14f2a81d0f3b1937c472a8f70a310d
dvghub <lonelecreature@gmail.com>
2020-01-06 15:50:07 +0100

    Initial commit.

---
+++ Organiser/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Organiser {
+    class Program {
+        static void Main(string[] args) {
+            var input = Console.ReadLine();
+            try {
+                var amount = int.Parse(input);
+                var list = CreateList(amount);
+                var list2 = list;
+                
+                foreach (var item in list) {
+                    Console.Write(item);
+                    Console.Write(" ");
+                }
+                Console.WriteLine("");
+                Console.WriteLine("");
+                
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+                Sorter.ShiftHigher(list);
+                stopwatch.Stop();
+                var shiftTime = stopwatch.Elapsed;
+                foreach (var item in list) {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine("");
+                Console.WriteLine(IsSorted(list) ? "List was sorted correctly." : "List was not sorted.");
+                Console.WriteLine("");
+                
+                stopwatch.Start();
+                Sorter.Rotate(list2, 0, list.Count-1);
+                stopwatch.Stop();
+                var rotateTime = stopwatch.Elapsed;
+                foreach (var item in list2) {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine("");
+                Console.WriteLine(IsSorted(list2) ? "List was sorted correctly." : "List was not sorted.");
+                Console.WriteLine("");
+                
+                Console.WriteLine("Shift time: " + shiftTime.Ticks);
+                Console.WriteLine("Rotate time: " + rotateTime.Ticks);
+            } catch (FormatException) {
+                Console.WriteLine("Input was not an int.");
+            }
+        }
+
+        private static List<int> CreateList(int amount) {
+            var list = new List<int>();
+            var rnd = new Random();
+            for (int i = 0; i < amount; i++) {
+                list.Add(rnd.Next(-99, 100));
+            }
+            return list;
+        }
+
+        private static bool IsSorted(List<int> list) {
+            for (int i = 0; i < list.Count-1; i++) {
+                if (list[i] > list[i+1]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Organiser/Sorter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Organiser {
+    public class Sorter {
+        public static List<int> ShiftHigher(List<int> list) {
+            var length = list.Count;
+            for (var j = 0; j < length-1; j++) {
+                for (var i = 0; i < length-1; i++) {
+                    if (list[i] < list[i + 1]) continue;
+                    var temp = list[i];
+                    list[i] = list[i+1];
+                    list[i + 1] = temp;
+                }
+            }
+            return list;
+        }
+        
+        public static void Rotate(List<int> list, int low, int high) {
+            if (low >= high) return;
+            var split = Partition(list, low, high);
+
+            Rotate(list, low, split - 1);
+            Rotate(list, split + 1, high);
+        }
+        
+        private static int Partition(List<int> list, int low, int high) {
+            var pivot = list[high];
+            var index = (low - 1);
+            int temp;
+
+            for (var j = low; j < high; j++) {
+                if (list[j] > pivot) continue;
+                index++;
+
+                temp = list[index];
+                list[index] = list[j];
+                list[j] = temp;
+            }
+
+            temp = list[index + 1];
+            list[index + 1] = list[high];
+            list[high] = temp;
+
+            return index + 1;
+        }
+    }
+}
