bd6d81ed902f462f2975071c37b019636372d49e
dvghub <lonelecreature@gmail.com>
2020-01-07 08:41:08 +0100

    Added BornToMove

---
+++ BornToMove/DatabaseConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace BornToMove {
+    public class DatabaseConnection {
+        private DatabaseConnection() { }
+        public MySqlConnection Connection { get; private set; } = null;
+        public string DatabaseName { get; set; } = string.Empty;
+
+        private static DatabaseConnection _instance = null;
+        public static DatabaseConnection Instance() {
+            if (_instance == null) _instance = new DatabaseConnection();
+            return _instance;
+        }
+
+        public bool IsConnected() {
+            if (Connection == null) {
+                if (string.IsNullOrEmpty(DatabaseName)) return false;
+                var config = string.Format("Server=localhost; database={0}; UID=sql_manager; password=lookatmeimastrongpassword", DatabaseName);
+                Connection = new MySqlConnection(config);
+                Connection.Open();
+            }
+            return true;
+        }
+
+        public void Close()
+        {
+            Connection.Close();
+        }
+    }
+}
---
+++ BornToMove/Move.cs
@@ -0,0 +1,8 @@
+﻿namespace BornToMove {
+    public class Move {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+    }
+}
