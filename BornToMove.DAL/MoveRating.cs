66094ad52c19f13cfd82087115887145e4d6856d
dvghub <dieuwer.greevenbroek@gmail.com>
2020-01-08 18:31:26 +0100

    Added rating class

---
+++ BornToMove.DAL/MoveRating.cs
@@ -0,0 +1,7 @@
+﻿namespace BornToMove.DAL {
+    public class MoveRating {
+        public Move Move { get; set; }
+        public int Rating { get; set; }
+        public int Intensity { get; set; }
+    }
+}
